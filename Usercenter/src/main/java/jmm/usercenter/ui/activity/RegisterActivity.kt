f9157cc60499890b0868359f6142961ef568429e
Leeyip <leeyip@163.com>
2018-04-08 16:27:45 +0800

    模块化的框架搭建 day01

---
+++ Usercenter/src/main/java/jmm/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package jmm.usercenter.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import jmm.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
